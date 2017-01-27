09f431e4e1811c839925f71eafc77b680597e829
asady1 <asady1@jhu.edu>
2017-01-27 16:56:28 -0500

    Add files via upload

---
+++ runSFcalcGH.sh
@@ -0,0 +1,137 @@
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuDiJet170Mu5_run2016G-H_pt250_DoubleBL.root pt7 DoubleBL Double results doubleBLpt1 0 250
+cp -r pics/ results_final/doubleBLpt7 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuDiJet170Mu5_run2016G-H_pt250_DoubleBM1.root pt7 DoubleBM1 Double results doubleBLpt1 1 250
+cp -r pics/ results_final/doubleBM1pt7 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuDiJet170Mu5_run2016G-H_pt250_DoubleBM2.root pt7 DoubleBM2 Double results doubleBLpt1 2 250
+cp -r pics/ results_final/doubleBM2pt7 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuDiJet170Mu5_run2016G-H_pt250_DoubleBH.root pt7 DoubleBH DoubleT results doubleBLpt1 3 250
+cp -r pics/ results_final/doubleBHpt7 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuDiJet170Mu5_run2016G-H_pt250_DoubleBL.root pt5 DoubleBL Double results doubleBLpt1 4 250
+cp -r pics/ results_final/doubleBLpt5L 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuDiJet170Mu5_run2016G-H_pt250_DoubleBM1.root pt5 DoubleBM1 Double results doubleBLpt1 5 250
+cp -r pics/ results_final/doubleBM1pt5L 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuDiJet170Mu5_run2016G-H_pt250_DoubleBM2.root pt5 DoubleBM2 Double results doubleBLpt1 6 250
+cp -r pics/ results_final/doubleBM2pt5L 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuDiJet170Mu5_run2016G-H_pt250_DoubleBH.root pt5 DoubleBH DoubleT results doubleBLpt1 7 250
+cp -r pics/ results_final/doubleBHpt5L 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBL.root pt5 DoubleBL Double results doubleBLpt1 8 350
+cp -r pics/ results_final/doubleBLpt5H 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBM1.root pt5 DoubleBM1 Double results doubleBLpt1 9 350
+cp -r pics/ results_final/doubleBM1pt5H 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBM2.root pt5 DoubleBM2 Double results doubleBLpt1 10 350
+cp -r pics/ results_final/doubleBM2pt5H 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBH.root pt5 DoubleBH DoubleT results doubleBLpt1 11 350
+cp -r pics/ results_final/doubleBHpt5H 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBL.root pt4 DoubleBL Double results doubleBLpt1 12 350
+cp -r pics/ results_final/doubleBLpt4 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBM1.root pt4 DoubleBM1 Double results doubleBLpt1 13 350
+cp -r pics/ results_final/doubleBM1pt4 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBM2.root pt4 DoubleBM2 Double results doubleBLpt1 14 350
+cp -r pics/ results_final/doubleBM2pt4 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBH.root pt4 DoubleBH DoubleT results doubleBLpt1 15 350
+cp -r pics/ results_final/doubleBHpt4 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBL.root pt6 DoubleBL Double results doubleBLpt1 16 350
+cp -r pics/ results_final/doubleBLpt6 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBM1.root pt6 DoubleBM1 Double results doubleBLpt1 17 350
+cp -r pics/ results_final/doubleBM1pt6 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBM2.root pt6 DoubleBM2 Double results doubleBLpt1 18 350
+cp -r pics/ results_final/doubleBM2pt6 
+./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_HLTBTagMuAK8Jet300Mu5_run2016G-H_pt350_DoubleBH.root pt6 DoubleBH DoubleT results doubleBLpt1 19 350
+cp -r pics/ results_final/doubleBHpt6 
+
+#echo"double L5"
+#./test normGH/Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_DoubleBL.root pt1 DoubleBL Double results doubleBLpt1 0
+#cp -r pics/ results_final/doubleBLpt1 
+
+#echo"double L4"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_DoubleBL.root pt6 DoubleBL Double results doubleBLpt6 1
+#cp -r pics/ results_final/doubleBLpt6 
+
+#echo"double M15"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_DoubleBM1.root pt1 DoubleBM1 Double results doubleBM1pt1 2 
+#cp -r pics/ results_final/doubleBM1pt1
+
+#echo"double M14"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_DoubleBM1.root pt6 DoubleBM1 Double results doubleBM1pt6 3
+#cp -r pics/ results_final/doubleBM1pt6 
+
+#echo"double M25"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_DoubleBM2.root pt1 DoubleBM2 Double results doubleBM2pt1 4
+#cp -r pics/ results_final/doubleBM2pt1 
+
+#echo"double M24"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_DoubleBM2.root pt6 DoubleBM2 Double results doubleBM2pt6 5
+#cp -r pics/ results_final/doubleBM2pt6 
+
+#echo"double H5"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_DoubleBH.root pt1 DoubleBH Double results doubleBHpt1 6
+#cp -r pics/ results_final/doubleBHpt1  
+
+#echo"double H4"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_DoubleBH.root pt6 DoubleBH Double results doubleBHpt6 7
+#cp -r pics/ results_final/doubleBHpt6 
+
+#echo"double L5"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_350to450_DoubleBL.root pt0 DoubleBL Double results doubleBLpt0 0
+#cp -r pics/ results_final/doubleBLpt0 
+
+#echo"double L4"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_350to450_DoubleBL.root pt5 DoubleBL Double results doubleBLpt5 1
+#cp -r pics/ results_final/doubleBLpt5 
+
+#echo"double M15"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_350to450_DoubleBM1.root pt0 DoubleBM1 Double results doubleBM1pt0 2 
+#cp -r pics/ results_final/doubleBM1pt0
+
+#echo"double M14"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_350to450_DoubleBM1.root pt5 DoubleBM1 Double results doubleBM1pt5 3
+#cp -r pics/ results_final/doubleBM1pt5 
+
+#echo"double M25"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_350to450_DoubleBM2.root pt0 DoubleBM2 Double results doubleBM2pt0 4
+#cp -r pics/ results_final/doubleBM2pt0 
+
+#echo"double M24"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_350to450_DoubleBM2.root pt5 DoubleBM2 Double results doubleBM2pt5 5
+#cp -r pics/ results_final/doubleBM2pt5 
+
+#echo"double H5"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_350to450_DoubleBH.root pt0 DoubleBH Double results doubleBHpt0 6
+#cp -r pics/ results_final/doubleBHpt0  
+
+#echo"double H4"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigAK8Jet300_2016G_350to450_DoubleBH.root pt5 DoubleBH Double results doubleBHpt5 7
+#cp -r pics/ results_final/doubleBHpt5 
+
+#echo"double L5"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigDitJet170_2016G_250to350_DoubleBL.root pt7 DoubleBL Double results doubleBLpt7 0
+#cp -r pics/ results_final/doubleBLpt7 
+
+#echo"double L4"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigDitJet170_2016G_250to350_DoubleBL.root pt5 DoubleBL Double results doubleBLpt5 1
+#cp -r pics/ results_final/doubleBLpt5_250 
+
+#echo"double M15"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigDitJet170_2016G_250to350_DoubleBM1.root pt7 DoubleBM1 Double results doubleBM1pt7 2 
+#cp -r pics/ results_final/doubleBM1pt7
+
+#echo"double M14"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigDitJet170_2016G_250to350_DoubleBM1.root pt5 DoubleBM1 Double results doubleBM1pt5 3
+#cp -r pics/ results_final/doubleBM1pt5_250
+
+#echo"double M25"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigDitJet170_2016G_250to350_DoubleBM2.root pt7 DoubleBM2 Double results doubleBM2pt7 4
+#cp -r pics/ results_final/doubleBM2pt7 
+
+#echo"double M24"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigDitJet170_2016G_250to350_DoubleBM2.root pt5 DoubleBM2 Double results doubleBM2pt5 5
+#cp -r pics/ results_final/doubleBM2pt5_250
+
+#echo"double H5"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigDitJet170_2016G_250to350_DoubleBH.root pt7 DoubleBH DoubleT results doubleBHpt7 6
+#cp -r pics/ results_final/doubleBHpt7  
+
+#echo"double H4"
+#./test Final_histograms_btagval_optimized_doublemu_BTagMu_QCDMuEnriched_dataWithMCJP_v2_12p9_correctedJP_v3_BTagMuTrigDitJet170_2016G_250to350_DoubleBH.root pt5 DoubleBH Double results doubleBHpt5 7
+#cp -r pics/ results_final/doubleBHpt5_250
+
